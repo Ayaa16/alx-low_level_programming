e7936e8a70c2e4d9a6b934b0909e7a170936c71c
Ayaa16 <ayamoustafa89@yahoo.com>
2023-04-24 10:22:19 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 /**
- *puts function
- *main - entry point
- *Return: Always 0 (Success)
+ * main - entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char myC[] = "Programming is like building a multilingual puzzle";
+	char myC[] = (\"Programming is like building a multilingual puzzle);
 puts(myC);
 return (0);
 }
