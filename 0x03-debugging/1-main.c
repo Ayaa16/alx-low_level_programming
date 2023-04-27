9eb5414725ff8c52e461a6c9a1a0f9a09404ad61
Ayaa16 <ayamoustafa89@yahoo.com>
2023-04-27 12:21:16 -0700

    done

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Description: make infinite loop and stoped by remove while
+* @\\o/\n: parameter to make infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+/**
+*     while (i < 10)
+*    {
+*            putchar(i);
+*    }
+*/
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
+}
