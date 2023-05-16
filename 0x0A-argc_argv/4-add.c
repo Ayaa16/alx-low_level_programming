bf9b977868bc5e01bd6acf7cfa67a4bd4c305a8a
Ayaa16 <ayamoustafa89@yahoo.com>
2023-05-16 11:25:31 -0700

    ok

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,21 +8,21 @@
  * @argv: array of arguments
  * Return: 0 (Success), 1 (Error)
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
 int result, a, b;
 
-if ((isdigit(argv[1]) == 0 || isdigit(argv[2]) == 0))
-{
-printf("Error\n");
-return (1);
-}
-else
+if ((isdigit(argv[argc]) && isdigit(argv[argc]))
 {
 a = atoi(argv[1]);
 b = atoi(argv[2]);
 result = a + b;
 printf("%d\n", result);
 return (0);
+}
+else if ((isdigit(argv[argc]) || isdigit(argv[argc]))
+{
+printf("Error\n");
+return (1);
 }
 }
